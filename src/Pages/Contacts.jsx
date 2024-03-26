0a610d0c644402219385ce2a98e9d49db848b52a
Orlando Ferazzani <orli@Orlandos-MacBook-Air.local>
2024-03-26 19:57:08 +0100

    prima parte contatti

--- src/Pages/Contacts.jsx
+++ src/Pages/Contacts.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import ContactForm from '../components/ContactForm'
+import Map from '../components/Map'
 
 function Contacts() {
     return (
-        <div>Contacts</div>
+        <div class='contacts-container'>
+            <div class='form'>
+                <ContactForm />
+            </div>
+            <div class='map'>
+                <Map />
+            </div>
+        </div>
     )
 }
 
