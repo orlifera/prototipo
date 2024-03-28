59118ab85133c41891487e6c388532c92ae92669
Orlando Ferazzani <orli@Orlandos-MacBook-Air.local>
2024-03-28 15:16:42 +0100

    new map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 
 function Map() {
     return (
-        <>
-            {/* to be revised */ }
-            <div className='separator'>
-                <iframe width="100%" height="800" src="https://maps.google.com/maps?width=100%25&amp;height=800&amp;hl=en&amp;q=Sinna's%20social%20club+(Sinna's%20social%20Club)&amp;t=k&amp;z=15&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+        <div className='map-container'>
+            <h1 className='title'>Ci trovi qua!</h1>
+            <p className='paragraph'>Vieni a trovarci, siamo aperti tutti i giorni dalle 10:00 alle 22:00.</p>
+            <div>
+                <iframe width='100%' height='100%' src="https://api.mapbox.com/styles/v1/orlifera/clubb5ges004601qq2rs0h3bu.html?title=false&access_token=pk.eyJ1Ijoib3JsaWZlcmEiLCJhIjoiY2x1YmIwNXIzMHN3NTJqcW54M2ZheHF5diJ9.Qr1sq3jmJBh-hN4nAK4x_g&zoomwheel=false#15.83/45.792094/11.429937/-37.1/40" ></iframe>
             </div>
-        </>
+        </div>
     )
 }
 
