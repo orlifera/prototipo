2ea2d17f8b2009b2cc9e840bde845248e6aaf286
Matteo <matteo.tiozzo.1@studenti.unipd.it>
2024-04-04 14:34:13 +0200

    fix responsive menù

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -3,10 +3,7 @@ import Prodotti from '../components/Prodotti';
 
 function Menu() {
   return (
-    <div className="menu">
-        <h2>Menu del giorno</h2>
-        <Prodotti />
-    </div>
+    <Prodotti />
   );
 }
 
--- src/components/Prodotti.jsx
+++ src/components/Prodotti.jsx
@@ -8,7 +8,8 @@ import Piatto4 from '../assets/images/paninesopressa.png'
 
 function Prodotti() {
   return (
-
+      <div className="menu">
+        <h2>Menu del giorno</h2>
       <div className="card">
         <img src={Piatto1} alt="Piatto spaghetti" />
         <div className="card-info">
@@ -17,8 +18,71 @@ function Prodotti() {
           <p>Prezzo: €10</p>
         </div>
       </div>
-      
-
+      <div className="card">
+        <img src={Piatto1} alt="Piatto 2" />
+        <div className="card-info">
+          <h3>Piatto 2</h3>
+          <p>Descrizione del piatto 2.</p>
+          <p>Prezzo: €12</p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Piatto1} alt="Piatto 3" />
+        <div className="card-info">
+          <h3>Piatto 3</h3>
+          <p>Descrizione del piatto 3.</p>
+          <p>Prezzo: €15</p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Piatto2} alt="Piatto 3" />
+        <div className="card-info">
+          <h3>Piatto 4</h3>
+          <p>Descrizione del piatto 3.</p>
+          <p>Prezzo: €7</p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Piatto2} alt="Piatto 3" />
+        <div className="card-info">
+          <h3>Piatto 5</h3>
+          <p>Descrizione del piatto 3.</p>
+          <p>Prezzo: €15</p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Piatto2} alt="Piatto 3" />
+        <div className="card-info">
+          <h3>Piatto 6</h3>
+          <p>Descrizione del piatto 3.</p>
+          <p>Prezzo: €15</p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Piatto3} alt="Piatto 3" />
+        <div className="card-info">
+          <h3>Piatto 7</h3>
+          <p>Descrizione del piatto 3.</p>
+          <p>Prezzo: €15</p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Piatto4} alt="Piatto 3" />
+        <div className="card-info">
+          <h3>Piatto 8</h3>
+          <p>Descrizione del piatto 3.</p>
+          <p>Prezzo: €15</p>
+        </div>
+      </div>
+      <div className="card">
+        <img src={Piatto4} alt="Piatto 3" />
+        <div className="card-info">
+          <h3>Piatto 9</h3>
+          <p>Descrizione del piatto 3.</p>
+          <p>Prezzo: €15</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
