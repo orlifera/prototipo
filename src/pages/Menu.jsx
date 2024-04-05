1dc3c154f77df3da06013ca93bc53c6c88f8cc5e
unknown <oferazzani125@gmail.com>
2024-04-05 16:29:25 +0200

    odio la mia vita

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -7,31 +7,31 @@ import panino from '../assets/images/paninesopressa.png';
 
 
 function Menu() {
-    return (
-        <div className="menu">
-            <Prodotti
-                nome_piatto="Spaghetti al pomodoro"
-                descrizione_piatto="Pasta con pomodoro"
-                prezzo_piatto="€10"
-                path={ spaghetti }
-                alt_image="Spaghetti al pomodoro"
-            />
-            <Prodotti
-                nome_piatto="Baccalà"
-                descrizione_piatto="Baccalà"
-                prezzo_piatto="€12"
-                path={ baccalà }
-                alt_image="Baccalà"
-            />
-            {/* <Prodotti 
+  return (
+    <div className="menu">
+      <Prodotti
+        nome_piatto="Spaghetti al pomodoro"
+        descrizione_piatto="Pasta con pomodoro"
+        prezzo_piatto="€10"
+        path={ spaghetti }
+        alt_image="Spaghetti al pomodoro"
+      />
+      <Prodotti
+        nome_piatto="Baccalà"
+        descrizione_piatto="Baccalà"
+        prezzo_piatto="€12"
+        path={ baccalà }
+        alt_image="Baccalà"
+      />
+      {/* <Prodotti 
         nome_piatto="Panino e sopressa"
         descrizione_piatto="Miglior piatto del mondo" 
         prezzo_piatto="€5"
         path={panino}
         alt_image="Panino e sopressa"
       /> */}
-        </div>
-    );
+    </div>
+  );
 }
 
 export default Menu;
